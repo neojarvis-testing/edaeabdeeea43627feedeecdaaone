7553f3fc4e702041a076932c87e5bdf4378997bc
neojarvis <techadmin@iamneo.ai>
2024-01-11 07:48:00 +0000

    Submit-project-save-commit-1/11/2024--1:17:57-PM

--- Vasanth.java
+++ Vasanth.java
@@ -2,6 +2,7 @@ public class Vasanth {
     public static void main(String[] args) {
         System.out.println("wedd");
         System.out.println("w");
+        System.out.println("testing");
 
     }
 }
